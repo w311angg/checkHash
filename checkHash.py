01f9993e286d4fd56b91baaa3bbc0f09171d666e
RZ <30381083+w311ang@users.noreply.github.com>
2021-02-17 14:00:21 +0800

    Update checkHash.py

--- checkHash.py
+++ checkHash.py
@@ -5,7 +5,7 @@ a='number=2&uid=9891&card=1060'
 b=requests.post('http://behash.com/api/v2/workdata',data='uid=9891',cookies={'PHPSESSID':'5d7td4plvacn9v3k598rj9bj97'}).json()
 实际=b['data']['rate']
 在线=b['data']['online']
-
+print(目标,实际,在线)
 def check():
   if 实际<目标 or 在线<2:
     return True
