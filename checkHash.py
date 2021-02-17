e7118220f2e6cf991c1c7926c69415b8fd90214d
RZ <30381083+w311ang@users.noreply.github.com>
2021-02-17 13:12:54 +0800

    Add files via upload

---
+++ checkHash.py
@@ -0,0 +1,33 @@
+import requests
+a='number=2&uid=9891&card=1060'
+目标=requests.post('http://behash.com/api/v2/CalByCard',data=a).json()['data']['incomeNum']
+b=requests.post('http://behash.com/api/v2/workdata',data='uid=9891',cookies={'PHPSESSID':'5d7td4plvacn9v3k598rj9bj97'}).json()
+实际=b['data']['rate']
+在线=b['data']['online']
+
+def check():
+  if 实际>目标 or 在线<2:
+    return True
+  else:
+    return False
+
+
+if check():
+  import smtplib
+  from email.mime.text import MIMEText
+  from email.utils import formataddr
+   
+  my_sender='w311ang@qq.com'    # 发件人邮箱账号
+  my_pass = 'fetewmcynscaddae'              # 发件人邮箱密码
+  my_user='w311ang@qq.com'      # 收件人邮箱账号，我这边发送给自己
+  def mail():
+      msg=MIMEText('应挖'+str(目标)+'，实挖'+str(实际)+'，在线'+str(在线),'plain','utf-8')
+      msg['From']=formataddr(["checkHash",my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+      msg['To']=formataddr(["WG",my_user])              # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+      msg['Subject']="哈希宝挖矿不达标"                # 邮件的主题，也可以说是标题
+   
+      server=smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25
+      server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
+      server.sendmail(my_sender,[my_user,],msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+      server.quit()  # 关闭连接
+  mail()
