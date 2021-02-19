42e5792665509aaa5b3f5994de8ac375f585c83e
RZ <30381083+w311ang@users.noreply.github.com>
2021-02-19 12:05:30 +0800

    Update checkHash.py

--- checkHash.py
+++ checkHash.py
@@ -2,6 +2,8 @@ import requests
 import os
 a='number=2&uid=9891&card=1060'
 目标=requests.post('http://behash.com/api/v2/CalByCard',data=a).json()['data']['incomeNum']
+e='number=1&uid=9891&card=1060'
+1目标=requests.post('http://behash.com/api/v2/CalByCard',data=e).json()['data']['incomeNum']
 b=requests.post('http://behash.com/api/v2/workdata',data='uid=9891',cookies={'PHPSESSID':'5d7td4plvacn9v3k598rj9bj97'}).json()
 实际=b['data']['rate']
 在线=b['data']['online']
@@ -16,7 +18,13 @@ print(发送内容)
 
 def check():
   if 实际<目标 or 在线<2:
-    print('挖矿不达标')
+    if 实际<1目标:
+      发送主题='⚠单台电脑哈希宝挖矿不达标'
+      发送内容='应挖'+str(目标)+'，实挖'+str(实际)+'('+str(实际-1目标)+')'+'，在线'+str(在线)+'\n'+在线情况
+      print(发送主题)
+      return True
+    发送主题='哈希宝挖矿不达标'
+    print(发送主题)
     return True
   else:
     print('挖矿达标')
@@ -34,7 +42,7 @@ if check():
       msg=MIMEText(发送内容,'plain','utf-8')
       msg['From']=formataddr(["checkHash",my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
       msg['To']=formataddr(["WG",my_user])              # 括号里的对应收件人邮箱昵称、收件人邮箱账号
-      msg['Subject']="哈希宝挖矿不达标"                # 邮件的主题，也可以说是标题
+      msg['Subject']=发送主题                # 邮件的主题，也可以说是标题
    
       server=smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25
       server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
