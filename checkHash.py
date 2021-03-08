074fc75e86edcbabaa26dee9b13a795b857c40ff
RZ <30381083+w311ang@users.noreply.github.com>
2021-03-08 21:38:34 +0800

    Update checkHash.py

--- checkHash.py
+++ checkHash.py
@@ -18,7 +18,7 @@ on=os.getenv('on')
 if on=='schedule':
   time.sleep(wait*60)
 h=session.post('http://behash.com/api/v2/login',data={'password':os.getenv('password'),'account':os.getenv('account')})
-print(h.text)
+#print(h.text)
 uid=h.json()['data']['uid']
 a='number=2&uid=%s&card=1060'%(uid)
 #print(session.post('http://behash.com/api/v2/CalByCard',data=a).text)
