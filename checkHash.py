39e1502010727fe9043008a20d0f59377a3fff39
RZ <30381083+w311ang@users.noreply.github.com>
2021-03-09 20:19:21 +0800

    Update checkHash.py

--- checkHash.py
+++ checkHash.py
@@ -8,6 +8,7 @@ proxies = {
 }
 session = requests.Session()
 session.proxies.update(proxies)
+print(session.get('https://www.google.com/'))
 num=0
 with open('num.txt',mode='r') as f:
   num=int(f.read())
