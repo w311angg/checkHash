3047a42a5519498abc95894a5cf06b80ed1e8c04
RZ <30381083+w311ang@users.noreply.github.com>
2021-05-13 22:13:04 +0800

    Update checkHash.py

--- checkHash.py
+++ checkHash.py
@@ -44,7 +44,7 @@ e='number=1&uid=%s&card=1060'%(uid)
 f=session.post('http://app.behash.com/api/v2/CalByCard',data=e).json()['data']
 目标1=f['incomeNum']
 速度1=f['speed']
-phps=h.cookies['PHPSESSID']
+phps=session.cookies['PHPSESSID']
 b=session.post('http://app.behash.com/api/v2/workdata',data='uid=9891',cookies={'PHPSESSID':phps}).json()
 #print(b)
 实际=b['data']['rate']
