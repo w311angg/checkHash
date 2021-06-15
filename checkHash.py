55e98ccd03d0f7c2a1bf6aa77b392729f76e87be
RZ <30381083+w311ang@users.noreply.github.com>
2021-06-15 18:44:34 +0800

    Update checkHash.py

--- checkHash.py
+++ checkHash.py
@@ -139,7 +139,7 @@ if check() and (not num%2):
 today=time.strftime("%d", time.localtime())
 #if today=='19' and (not rewarded):
 if True:
-  account=os.getenv('accountt')
+  account=os.getenv('raccount')
   num=session.post('http://app.behash.com/api/v2/center',data={'uid':uid}).json()['data']['reward']
   num=0.01
   reward=session.post('http://app.behash.com/api/v2/withdrawing',data={'symbol':'reward','uid':uid,'num':num,'type':1,'account':account}).json()
